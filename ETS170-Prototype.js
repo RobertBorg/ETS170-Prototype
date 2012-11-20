f700413a3cc64b8d327a2084f31a96b0e42de4b1
Robert Borg <robert.borg@gmail.com>
2012-11-20 12:45:35 +0100

    added parties

--- ETS170-Prototype.js
+++ ETS170-Prototype.js
@@ -1,16 +1,23 @@
-Patries = new Meteor.Collection("parties"); //name
+Parties = new Meteor.Collection("parties"); //name
 Candidates = new Meteor.Collection("candidates"); //name, party
 
 if (Meteor.isClient) {
-  Template.hello.greeting = function () {
-    return "Welcome to ETS170-Prototype. YEY";
-  };
 
-  Template.hello.events({
-    'click input' : function () {
-      // template data, if any, is available in 'this'
-      if (typeof console !== 'undefined')
-        console.log("You pressed the button");
+  Template.party_list.all = function () {
+    return Parties.find();
+  }
+
+  Template.party_list.is_selected = function () {
+    if(Session.get("selected_party") === this._id){
+      return "party_selected";
+    } else {
+      return "";
+    }
+  }
+
+  Template.party_list.events({
+    'click ul.party_list>li' : function () {
+      Session.set("selected_party",this._id);
     }
   });
 }
